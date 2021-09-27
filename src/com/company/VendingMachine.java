c00da980dc109fcc245ba606f9aeeebf30091008
krist <kristinapak55@gmail.com>
2021-09-27 17:41:06 +0600

    reformat code

--- src/com/company/VendingMachine.java
+++ src/com/company/VendingMachine.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class VendingMachine {
     static int money = 0;
 
-    public static void menu(){
+    public static void menu() {
         System.out.println("              В автомате доступны");
         String f = "|%25s|%10s|%10s|";
         System.out.println("+-------------------------+----------+----------+");
         System.out.println(String.format(f, "товар", "цена", "количество"));
         System.out.println("+-------------------------+----------+----------+");
-        System.out.println(String.format(f,GOODS.SODA.getDescription(), GOODS.SODA.getPrice(), GOODS.SODA.getAmount()));
+        System.out.println(String.format(f, GOODS.SODA.getDescription(), GOODS.SODA.getPrice(), GOODS.SODA.getAmount()));
         System.out.println(String.format(f, GOODS.CANDY.getDescription(), GOODS.CANDY.getPrice(), GOODS.CANDY.getAmount()));
         System.out.println(String.format(f, GOODS.WATER.getDescription(), GOODS.WATER.getPrice(), GOODS.WATER.getAmount()));
         System.out.println(String.format(f, GOODS.CHEEPS.getDescription(), GOODS.CHEEPS.getPrice(), GOODS.CHEEPS.getAmount()));
@@ -39,19 +39,19 @@ public class VendingMachine {
             System.out.println("Что хотите купить?(введите кнопку)");
             String frm = "|%25s|%5s|%3s|%3s|";
             System.out.println("+-------------------------+-----+---+---+");
-            System.out.println(String.format(frm,GOODS.SODA.getDescription() ,GOODS.SODA.getPrice() , " - " , GOODS.SODA.getButton()));
-            System.out.println(String.format(frm,GOODS.CANDY.getDescription() , GOODS.CANDY.getPrice(), " - " , GOODS.CANDY.getButton()));
-            System.out.println(String.format(frm,GOODS.WATER.getDescription() , GOODS.WATER.getPrice(), " - " , GOODS.WATER.getButton()));
-            System.out.println(String.format(frm,GOODS.CHEEPS.getDescription() , GOODS.CHEEPS.getPrice(), " - " , GOODS.CHEEPS.getButton()));
-            System.out.println(String.format(frm,GOODS.CHOCOLATE_BAR.getDescription() , GOODS.CHOCOLATE_BAR.getPrice(), " - " , GOODS.CHOCOLATE_BAR.getButton()));
+            System.out.println(String.format(frm, GOODS.SODA.getDescription(), GOODS.SODA.getPrice(), " - ", GOODS.SODA.getButton()));
+            System.out.println(String.format(frm, GOODS.CANDY.getDescription(), GOODS.CANDY.getPrice(), " - ", GOODS.CANDY.getButton()));
+            System.out.println(String.format(frm, GOODS.WATER.getDescription(), GOODS.WATER.getPrice(), " - ", GOODS.WATER.getButton()));
+            System.out.println(String.format(frm, GOODS.CHEEPS.getDescription(), GOODS.CHEEPS.getPrice(), " - ", GOODS.CHEEPS.getButton()));
+            System.out.println(String.format(frm, GOODS.CHOCOLATE_BAR.getDescription(), GOODS.CHOCOLATE_BAR.getPrice(), " - ", GOODS.CHOCOLATE_BAR.getButton()));
             System.out.println("+-------------------------+-----+---+---+");
             int bg = s.nextInt();
-            if (bg == 1){
+            if (bg == 1) {
                 System.out.println("          Вы купили газировку");
                 System.out.println("+-------------------------+----------+----------+");
                 System.out.println(String.format(f, "товар", "цена", "количество"));
                 System.out.println("+-------------------------+----------+----------+");
-                System.out.println(String.format(f,GOODS.SODA.getDescription(), GOODS.SODA.getPrice(), GOODS.SODA.getAmount() - 1));
+                System.out.println(String.format(f, GOODS.SODA.getDescription(), GOODS.SODA.getPrice(), GOODS.SODA.getAmount() - 1));
                 System.out.println(String.format(f, GOODS.CANDY.getDescription(), GOODS.CANDY.getPrice(), GOODS.CANDY.getAmount()));
                 System.out.println(String.format(f, GOODS.WATER.getDescription(), GOODS.WATER.getPrice(), GOODS.WATER.getAmount()));
                 System.out.println(String.format(f, GOODS.CHEEPS.getDescription(), GOODS.CHEEPS.getPrice(), GOODS.CHEEPS.getAmount()));
@@ -64,8 +64,8 @@ public class VendingMachine {
                 System.out.println("+-------------------------+----------+----------+");
                 System.out.println(String.format(f, "товар", "цена", "количество"));
                 System.out.println("+-------------------------+----------+----------+");
-                System.out.println(String.format(f,GOODS.SODA.getDescription(), GOODS.SODA.getPrice(), GOODS.SODA.getAmount() ));
-                System.out.println(String.format(f, GOODS.CANDY.getDescription(), GOODS.CANDY.getPrice(), GOODS.CANDY.getAmount()-1));
+                System.out.println(String.format(f, GOODS.SODA.getDescription(), GOODS.SODA.getPrice(), GOODS.SODA.getAmount()));
+                System.out.println(String.format(f, GOODS.CANDY.getDescription(), GOODS.CANDY.getPrice(), GOODS.CANDY.getAmount() - 1));
                 System.out.println(String.format(f, GOODS.WATER.getDescription(), GOODS.WATER.getPrice(), GOODS.WATER.getAmount()));
                 System.out.println(String.format(f, GOODS.CHEEPS.getDescription(), GOODS.CHEEPS.getPrice(), GOODS.CHEEPS.getAmount()));
                 System.out.println(String.format(f, GOODS.CHOCOLATE_BAR.getDescription(), GOODS.CHOCOLATE_BAR.getPrice(), GOODS.CHOCOLATE_BAR.getAmount()));
@@ -77,9 +77,9 @@ public class VendingMachine {
                 System.out.println("+-------------------------+----------+----------+");
                 System.out.println(String.format(f, "товар", "цена", "количество"));
                 System.out.println("+-------------------------+----------+----------+");
-                System.out.println(String.format(f,GOODS.SODA.getDescription(), GOODS.SODA.getPrice(), GOODS.SODA.getAmount() ));
+                System.out.println(String.format(f, GOODS.SODA.getDescription(), GOODS.SODA.getPrice(), GOODS.SODA.getAmount()));
                 System.out.println(String.format(f, GOODS.CANDY.getDescription(), GOODS.CANDY.getPrice(), GOODS.CANDY.getAmount()));
-                System.out.println(String.format(f, GOODS.WATER.getDescription(), GOODS.WATER.getPrice(), GOODS.WATER.getAmount()-1));
+                System.out.println(String.format(f, GOODS.WATER.getDescription(), GOODS.WATER.getPrice(), GOODS.WATER.getAmount() - 1));
                 System.out.println(String.format(f, GOODS.CHEEPS.getDescription(), GOODS.CHEEPS.getPrice(), GOODS.CHEEPS.getAmount()));
                 System.out.println(String.format(f, GOODS.CHOCOLATE_BAR.getDescription(), GOODS.CHOCOLATE_BAR.getPrice(), GOODS.CHOCOLATE_BAR.getAmount()));
                 System.out.println("+-------------------------+----------+----------+");
@@ -90,10 +90,10 @@ public class VendingMachine {
                 System.out.println("+-------------------------+----------+----------+");
                 System.out.println(String.format(f, "товар", "цена", "количество"));
                 System.out.println("+-------------------------+----------+----------+");
-                System.out.println(String.format(f,GOODS.SODA.getDescription(), GOODS.SODA.getPrice(), GOODS.SODA.getAmount() ));
+                System.out.println(String.format(f, GOODS.SODA.getDescription(), GOODS.SODA.getPrice(), GOODS.SODA.getAmount()));
                 System.out.println(String.format(f, GOODS.CANDY.getDescription(), GOODS.CANDY.getPrice(), GOODS.CANDY.getAmount()));
                 System.out.println(String.format(f, GOODS.WATER.getDescription(), GOODS.WATER.getPrice(), GOODS.WATER.getAmount()));
-                System.out.println(String.format(f, GOODS.CHEEPS.getDescription(), GOODS.CHEEPS.getPrice(), GOODS.CHEEPS.getAmount()-1));
+                System.out.println(String.format(f, GOODS.CHEEPS.getDescription(), GOODS.CHEEPS.getPrice(), GOODS.CHEEPS.getAmount() - 1));
                 System.out.println(String.format(f, GOODS.CHOCOLATE_BAR.getDescription(), GOODS.CHOCOLATE_BAR.getPrice(), GOODS.CHOCOLATE_BAR.getAmount()));
                 System.out.println("+-------------------------+----------+----------+");
                 menu();
@@ -103,31 +103,31 @@ public class VendingMachine {
                 System.out.println("+-------------------------+----------+----------+");
                 System.out.println(String.format(f, "товар", "цена", "количество"));
                 System.out.println("+-------------------------+----------+----------+");
-                System.out.println(String.format(f,GOODS.SODA.getDescription(), GOODS.SODA.getPrice(), GOODS.SODA.getAmount() ));
+                System.out.println(String.format(f, GOODS.SODA.getDescription(), GOODS.SODA.getPrice(), GOODS.SODA.getAmount()));
                 System.out.println(String.format(f, GOODS.CANDY.getDescription(), GOODS.CANDY.getPrice(), GOODS.CANDY.getAmount()));
                 System.out.println(String.format(f, GOODS.WATER.getDescription(), GOODS.WATER.getPrice(), GOODS.WATER.getAmount()));
                 System.out.println(String.format(f, GOODS.CHEEPS.getDescription(), GOODS.CHEEPS.getPrice(), GOODS.CHEEPS.getAmount()));
-                System.out.println(String.format(f, GOODS.CHOCOLATE_BAR.getDescription(), GOODS.CHOCOLATE_BAR.getPrice(), GOODS.CHOCOLATE_BAR.getAmount()-1));
+                System.out.println(String.format(f, GOODS.CHOCOLATE_BAR.getDescription(), GOODS.CHOCOLATE_BAR.getPrice(), GOODS.CHOCOLATE_BAR.getAmount() - 1));
                 System.out.println("+-------------------------+----------+----------+");
                 menu();
                 actions();
             } else {
-                throw new  IllegalArgumentException("Введен неверный символ");
+                throw new IllegalArgumentException("Введен неверный символ");
             }
-        } else if(h == 3) {
+        } else if (h == 3) {
             System.out.println("Спасибо, приходите еще");
-        } else{
-            throw new  IllegalArgumentException("Введен неверный символ");
+        } else {
+            throw new IllegalArgumentException("Введен неверный символ");
         }
     }
 
-    public static void addMoney(){
+    public static void addMoney() {
         System.out.print(String.format("У вас сейчас %s на сколько хотите пополнить ", money));
         Scanner s = new Scanner(System.in);
 
         Integer moneyOne = s.nextInt();
         money = money + moneyOne;
-        System.out.println("-------------------Вы пополнили "+ money + "---------------");
+        System.out.println("-------------------Вы пополнили " + money + "---------------");
     }
 
 }
